315f8432306c64b526a19214e2d5587d01e37cd7
Marcelo Rodrigues <mr.leite73@gmail.com>
2020-06-26 14:14:05 -0300

    hasId removed

--- src/main/Program.java
+++ src/main/Program.java
@@ -27,11 +27,6 @@ public class Program {
 
 			System.out.print("Id: ");
 			int id = sc.nextInt();
-			while (hasId(list, id)) {
-				System.out.print("Id already taken. Try again: ");
-				id = sc.nextInt();
-			}
-			
 			System.out.print("Name: ");
 			sc.nextLine();
 			String name = sc.nextLine();
@@ -65,9 +60,5 @@ public class Program {
 				
 		sc.close(); 
 	}
-	
-	public static boolean hasId(List<Employee> list, int id) {
-		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
-		return emp != null;
-	}
+
 }
